5220dfc869f201bec3dcab930f0b56b72f14ff4c
Cheukdarsy <zodarsy@gmail.com>
2016-04-07 16:43:08 -0700

    remove the old file

--- jtest/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class test_list(models.Model):
-    pass
--- jtest/urls.py
+++
@@ -1,7 +0,0 @@
-# coding:utf-8
-from django.conf.urls import patterns, include, url
-from jtest.views import *
-
-urlpatterns = patterns('',
-                       url(r'^list/(\w+)/$','jtest.views.test_list', name='test_list'),
-                      )
