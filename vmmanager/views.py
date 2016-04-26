9289fe9b6383cf4012d010b1fef7f20242348717
Cheukdarsy <zodarsy@gmail.com>
2016-04-26 10:11:28 +0800

    Update views.py

--- vmmanager/views.py
+++ vmmanager/views.py
@@ -1,22 +1,18 @@
-#-*- coding:utf-8 -*-
-from django.utils import simplejson
-from django.core import serializers
-from jumpserver.api import *
-from jumpserver.models import Setting
-from jVM.models import userapply, userapply_confirm
-from juser.models import User
-from jVM.jvm_api import *
-from django.db.models import Q
-import datetime
+# -*- coding:utf-8 -*-
+
+from .jvm_api import *
+from .models import Application, Approvel
+
+
 # Create your views here.
 
 
 @require_role('admin')
 def VM_list(request):
     username = request.user.username
-    applylist = userapply.objects.filter(apply_status='SM').order_by('-id')
-    apply_confirm_list = userapply_confirm.objects.filter(
-        approving_status='AP').order_by('-approving_datetime')
+    applylist = Application.objects.filter(apply_status='SM').order_by('-id')
+    apply_confirm_list = Approvel.objects.filter(
+        appro_status='AP').order_by('-appro_date')
     applylist, p, applys, page_range, current_page, show_first, show_end = pages(applylist, request)
 
     return my_render('jvmanager/test_manage.html', locals(), request)
@@ -27,13 +23,12 @@ def show_apply_machinedetail(request):
     """ajax获取机器详细信息"""
     if request.method == "POST":
         id = int(request.POST.get('id', ''))
-        applydetail = userapply.objects.filter(id=id)
+        applydetail = Application.objects.filter(id=id)
         applydetail_dict = simplejson.dumps(applydetail, cls=QuerySetEncoder)
         return JsonResponse(applydetail_dict)
     else:
         error_dict = {'error': 'ajax not good'}
         return JsonResponse(error_dict)
-
 
 
 def modify_machine_detail(request, *call_args):
@@ -52,8 +47,12 @@ def modify_machine_detail(request, *call_args):
         saving_apply_status = 'SM'
         saving_datetime = datetime.datetime.now()
         try:
-            userapply.objects.filter(id=request_id).update(fun_type=saving_fun_type, os_type=saving_os_type,
-                                                           cpu=saving_cpu_num,env_type=saving_env_type,memory=saving_memory_num, data_disk=saving_data_disk,request_num=saving_apply_num,apply_date=saving_datetime)
+            Application.objects.filter(id=request_id).update(fun_type=saving_fun_type, os_type=saving_os_type,
+                                                             cpu=saving_cpu_num, env_type=saving_env_type,
+                                                             memory_gb=saving_memory_num, datadisk_gb=saving_data_disk,
+                                                             request_vm_num=saving_apply_num,
+                                                             apply_date=saving_datetime,
+                                                             apply_status=saving_apply_status)
         except Exception, e:
             raise e
         else:
@@ -71,6 +70,7 @@ def agree_apply(request):
     """
     if request.method == "POST":
         request_id = int(request.POST.get('request_id', ''))
+        application = Application.objects.get(id=request_id)
         approving_env_type = request.POST.get('confirm_env_type', '')
         approving_fun_type = request.POST.get('confirm_fun_type', '')
         approving_cpu_num = int(request.POST.get('confirm_cpu_num', ''))
@@ -81,15 +81,19 @@ def agree_apply(request):
         approving_status = "AP"
         approving_datetime = datetime.datetime.now()
         try:
-            userapply.objects.filter(id=request_id).update(apply_status=approving_status)
-            confirm_apply = userapply_confirm(request_id_id=request_id, approving_env_type=approving_env_type, approving_fun_type=approving_fun_type,
-                                              approving_cpu_num=approving_cpu_num, approving_memory_num=approving_memory_num, approving_os_type=approving_os_type, approving_data_disk=approving_data_disk, approving_appply_num=approving_apply_num, approving_status=approving_status, approving_datetime=approving_datetime)
+            Application.objects.filter(id=request_id).update(apply_status=approving_status)
+            confirm_apply = Approvel(application=application, appro_env_type=approving_env_type,
+                                     appro_fun_type=approving_fun_type,
+                                     appro_cpu=approving_cpu_num, appro_memory_gb=approving_memory_num,
+                                     appro_os_type=approving_os_type, appro_datadisk_gb=approving_data_disk,
+                                     appro_vm_num=approving_apply_num, appro_status=approving_status,
+                                     appro_date=approving_datetime)
             confirm_apply.save()
         except Exception, e:
             raise e
         else:
 
-            confirmlist = userapply_confirm.objects.filter(request_id_id=request_id)
+            confirmlist = Approvel.objects.filter(application=application)
             list_dict = simplejson.dumps(confirmlist, cls=QuerySetEncoder)
             return JsonResponse(list_dict)
     else:
@@ -104,7 +108,7 @@ def delete_apply(request):
         id = int(request.POST.get("id", ""))
         reason = request.POST.get("reason", "")
         try:
-            userapply.objects.filter(id=id).update(apply_status='RB')
+            Application.objects.filter(id=id).update(apply_status='RB')
         except Exception, e:
             raise e
         else:
@@ -148,8 +152,10 @@ def apply_machine(request):
             apply_status = "HD"
 
         try:
-            db_add_userapply(env_type=env_type, fun_type=fun_type, cpu=cpu, memory=memory, os_type=os_type, data_disk=data_disk, request_num=request_num,
-                             apply_status=apply_status, app_name=app_name, apply_reason=apply_reason, apply_date=datetime.datetime.now(), user=user)
+            db_add_userapply(env_type=env_type, fun_type=fun_type, cpu=cpu, memory_gb=memory, os_type=os_type,
+                             datadisk_gb=data_disk, request_vm_num=request_num,
+                             apply_status=apply_status, app_name=app_name, apply_reason=apply_reason,
+                             apply_date=datetime.datetime.now(), user=user)
         except ServerError:
             pass
         else:
@@ -164,7 +170,7 @@ def resource_view(request):
     用户资源概览
     """
     username = request.user.username
-    applylist = userapply.objects.exclude(apply_status="HD").order_by('-id')
+    applylist = Application.objects.exclude(apply_status="HD").order_by('-id')
     applylist, p, applys, page_range, current_page, show_first, show_end = pages(
         applylist, request)
     return my_render('jvmanager/resource_view.html', locals(), request)
@@ -172,7 +178,7 @@ def resource_view(request):
 
 @require_role('user')
 def saving_resource_view(request):
-    s_apply_list = userapply.objects.filter(apply_status="HD")
+    s_apply_list = Application.objects.filter(apply_status="HD")
     s_apply_list, p, s_applys, page_range, current_page, show_first, show_end = pages(
         s_apply_list, request)
     return my_render('jvmanager/savingresource_view.html', locals(), request)
@@ -184,7 +190,7 @@ def submit_saving_resource(request):
     if request.method == 'POST':
         id = int(request.POST.get('id', ''))
         try:
-            userapply.objects.filter(id=id).update(apply_status="SM")
+            Application.objects.filter(id=id).update(apply_status="SM")
         except Exception, e:
             raise e
         else:
@@ -197,63 +203,66 @@ def submit_saving_resource(request):
 
 
 """
-邱老板需填充函数如下
-"""
+    邱老板需填充函数如下
+    """
 #同意生成机器界面
 def generate_machine():
-	"""
-	前端返回一个ID列表{json}，然后根据ID去approv表获取相关参数生成机器，包括台数等
-	"""
-	pass
+    """
+        前端返回一个ID列表{json}，然后根据ID去approv表获取相关参数生成机器，包括台数等
+        """
+            pass
 
 def ajax_get_process():
-	"""
-	根据ID列表获取生成进度
-	"""
-	pass
+    """
+        根据ID列表获取生成进度
+        """
+            pass
 def ajax_get_generatestatus():
-	"""
-	根据ID列表获取生成日志
-	"""
-	pass
+    """
+        根据ID列表获取生成日志
+        """
+            pass
 def ajax_get_template():
-	"""
-	前端返回cluster名称clustername，获取对应的template
-	"""
-	pass
+    """
+        前端返回cluster名称clustername，获取对应的template
+        """
+            pass
 def ajax_get_cluster():
-	"""
-	动态获取集群信息，需返回所有可用集群名称以及权重比(CPU+内存＋存储)
-	"""
-	pass
+    """
+        动态获取集群信息，需返回所有可用集群名称以及权重比(CPU+内存＋存储)
+        """
+            pass
 def ajax_get_resource():
-	"""
-	获取资源称名称
-	"""
-	pass
+    """
+        获取资源称名称
+        """
+            pass
 def ajax_get_storage():
-	"""
-	获取相关集群的剩余容量，前端返回集群名称clustername，需返回剩余容量值及百分比
-	"""
-	pass
+    """
+        获取相关集群的剩余容量，前端返回集群名称clustername，需返回剩余容量值及百分比
+        """
+            pass
 #VM参数配置页面
 def ajax_add_IP():
-	"""
-	动态激活IP段
-	"""
-	pass
+    """
+        动态激活IP段
+        """
+            pass
 def ajax_get_IP():
-	"""
-	获取可用IP列表，用于下拉框选择
-	"""
-	pass
+    """
+        获取可用IP列表，用于下拉框选择
+        """
+            pass
 def ajax_add_template():
-	"""
-	添加template
-	"""
-	pass	
+    """
+        添加template
+        """
+            pass	
 def get_templates():
-	"""
-	获取所有模板信息
-	"""
-	pass
+    """
+        获取所有模板信息
+        """
+            pass
+
+
+
